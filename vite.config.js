41f4f1bfa8e02d48124f805f1b97ef1f293b6d3a
keerthika <keerthikachikki@gmail.com>
2026-02-10 12:53:44 +0530

    changed files

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"CounterApp" ,
+  base: '/CounterApp/', // needed for GitHub Pages
   plugins: [
-    react({
-      babel: {
-        plugins:[['babel-plugin-react-complier']],
-      },
-    }),
+    react() // no extra babel plugin needed
   ],
 })
